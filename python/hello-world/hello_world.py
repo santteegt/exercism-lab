15960713e35446839183f936248c7477c365ee61
Santiago Gonzalez <hernangt12re3@gmail.com>
2016-11-05 23:58:06 -0500

    Repo setup and Hello World for Python and Scala

---
+++ python/hello-world/hello_world.py
@@ -0,0 +1,7 @@
+#
+# Skeleton file for the Python "Hello World" exercise.
+#
+
+
+def hello(name=''):
+    return u'Hello, {}!'.format('World' if not name else name)
---
+++ scala/hello-world/src/main/scala/HelloWorld.scala
@@ -0,0 +1,8 @@
+/**
+  * Created by santteegt on 05/11/2016.
+  */
+object HelloWorld {
+
+  def hello(name: String = "World"): String = s"Hello, $name!"
+
+}
